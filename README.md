ba7b02e7e1cc46d0f5db146bfc22bef92c8bd1f8
Danielies367 <danielkristioutomo@gmail.com>
2022-05-16 02:30:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Linksme
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {  
+  darkMode: "class",
+  content: ['linksme.html'],
+  theme: {
+    extend: {
+      colors: {
+        secondary: '#B8B8B8',
+      },
+    },
+  },
+  plugins: [
+],
+}
